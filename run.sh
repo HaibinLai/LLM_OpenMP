42081fc1938c4af5813c3f56bd3220d5dc354294
HaibinLai <12211612@mail.sustech.edu.cn>
2025-03-28 21:39:02 +0000

    add run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+
+sudo apt update
+
+wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+bash Anaconda3-2024.10-1-Linux-x86_64.sh
+
+conda create -n LLM_OMP 
+conda activate LLM_OMP
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+export PATH="$PATH:/home/cc/.local/bin"
+pip install transformers
+
+
+huggingface-cli login
+pip install 'accelerate>=0.26.0'
